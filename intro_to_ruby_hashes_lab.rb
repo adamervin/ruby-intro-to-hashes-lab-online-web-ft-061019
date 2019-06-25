e90f90e0622ed0b4f916a0eafef5dd7bc8a0b4cf
Adam Ervin <adamjervin@gmail.com>
2019-06-25 18:09:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,8 +46,8 @@ def monopoly_with_third_tier
     :pieces =>4,
     :names =>
     {
-    :reading_railroad => 
-    {:pennsylvania_railroad => }
+    :reading_railroad => {}
+    
     
     },
     :rent_in_dollars =>  
